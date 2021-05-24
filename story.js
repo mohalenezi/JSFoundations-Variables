719f44f28f229000823decbe373ec3aadae235ab
mohalenezi <ot.m.alenezi@gmail.com>
2021-05-24 17:00:51 +0300

    task finished

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = "Mohammad alenezi";
+let age = 30;
+let hoppy = "treating people";
+console.log(`mr ${name}, in 33 years, you will be ${age +30} years old. by then you'll have decades of ${hoppy} experience . `)
